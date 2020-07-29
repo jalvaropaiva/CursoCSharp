a72aa0d0f033e0f826e14eeea87278c66cb9e7fd
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-07-29 14:54:30 -0300

    Exemplos WPF

---
+++ Exemplos/ExWpf/Triangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExWpf
+{
+    class Triangulo
+    {
+        private double b, h;
+        public Triangulo(double b, double h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+        public double CalcArea()
+        {
+            return b * h / 2;
+        }
+    }
+}
